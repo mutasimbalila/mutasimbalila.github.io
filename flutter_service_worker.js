e63ef6361a86bae4bed28a49a109d3d5aa9edc36
mutasimbalila <mutasimbalila@gmail.com>
2023-10-16 10:46:07 +0200

    new project

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -71,9 +71,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "4171c21db52fd1202c7a03d73b3be2b2
 "icons/Icon-maskable-192.png": "2818cec643beecd8eb138ccdfd869aae",
 "icons/Icon-maskable-512.png": "25e33057a6112d8baca06361e62942aa",
 "img/loading_code.gif": "e875ebc820b156f3c86184b6a26b84a9",
-"index.html": "5516d970baaa1ac275d7ffa55cc0dec9",
-"/": "5516d970baaa1ac275d7ffa55cc0dec9",
-"main.dart.js": "2eb121584ee251226d4cedbf21250639",
+"index.html": "ccb450ec919f6d284c74437a0142e30b",
+"/": "ccb450ec919f6d284c74437a0142e30b",
+"main.dart.js": "e48e807c6df3c9e84b713f979044f66e",
 "manifest.json": "69c4c678e6d585e697146578e5eb87e7",
 "styles.css": "3ff4b86a0be8b09eaacbd983e0ef8164",
 "version.json": "304c060da638f78d0869eebeb274b3ca"};
